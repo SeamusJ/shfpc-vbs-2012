5e0071bcdff9ac1ac10804156e796865334ade6e
Seamus Johnson <johnsons@censis.net>
2012-06-11 15:00:43 -0500

    Sessions moved to db.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Vbs::Application.config.session_store :cookie_store, key: '_vbs_session'
+#Vbs::Application.config.session_store :cookie_store, key: '_vbs_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# Vbs::Application.config.session_store :active_record_store
+Vbs::Application.config.session_store :active_record_store
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611182807) do
+ActiveRecord::Schema.define(:version => 20120611195625) do
 
   create_table "attendees", :force => true do |t|
     t.string   "first_name"
@@ -28,6 +28,24 @@ ActiveRecord::Schema.define(:version => 20120611182807) do
     t.string   "parent_zip"
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "hashed_password"
+    t.string   "email"
+    t.string   "salt"
+    t.datetime "created_at"
   end
 
 end
